204dec26f08a3ac8e1083747da9838b0e2c923a6
Ofir160 <ofir1ziv2@gmail.com>
2025-10-20 17:54:41 +0100

    I don't really know what happened github is being wierd

--- new-game-project/Scripts/Player/player_state.gd
+++ new-game-project/Scripts/Player/player_state.gd
@@ -9,10 +9,7 @@ static var double_jump_available : bool
 static var dashes_available : int
 static var dash_direction : Vector2
 static var superdash_queued : bool
-<<<<<<< HEAD
 
 func enter() -> void:
 	super()
 	parent.animated_sprite.play(animation_name)
-=======
->>>>>>> 229028fd398494695161919295fadf3a851ab744
